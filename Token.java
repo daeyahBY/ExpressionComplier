a8c3421ad98a7b944cc454760b436d94fcc4a802
Smoore300 <sm1002619@wcupa.edu>
2025-12-06 13:34:01 -0800

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,14 +1,13 @@
 public class Token {
-   
     //What kind of token
     TokenType type;
-    
+
     //Actual text of the token from input
     String value;
-    
+
     int pos; // position in the input string
 
-    //constructor 
+    //constructor
     public Token(TokenType type, String value, int pos) {
         this.type = type;
         this.value = value;
