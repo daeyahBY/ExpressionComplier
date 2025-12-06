9eb979dd59fc4a4704116529ac5b57ae122cdf43
Smoore300 <sm1002619@wcupa.edu>
2025-12-06 13:34:22 -0800

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -11,27 +11,27 @@ public class Lexer {
         this.i = 0;
     }
 
-    List<Token> tokenize() throws LexicalException{
+    List<Token> tokenize() throws LexicalException {
         List<Token> tokens = new ArrayList<>();
-        //
-        while(true) {
-            skipWhitespace(); // ignore whitespace
 
-            if (i >= length) { // End of input
+        while (true) {
+
+            skipWhitespace();  // ignore whitespace
+
+            if (i >= length) {  // end of input
                 tokens.add(new Token(TokenType.EOF, "", i));
                 break;
             }
 
-            // Check current character
-            char ch = text.charAt(i); 
+            char ch = text.charAt(i); // current character
 
-        
-            if (isDigit(ch)) { // number
+            // ----- NUMBERS -----
+            if (isDigit(ch)) {
                 String number = readNumber();
                 tokens.add(new Token(TokenType.NUMBER, number, i - number.length()));
-            } 
-            
-            // Operators and parentheses
+            }
+
+            // ----- OPERATORS -----
             else if (ch == '+') {
                 tokens.add(new Token(TokenType.PLUS, "+", i));
                 i++;
@@ -44,47 +44,59 @@ public class Lexer {
             } else if (ch == '/') {
                 tokens.add(new Token(TokenType.DIVIDE, "/", i));
                 i++;
+
+            // ----- PARENTHESES -----
             } else if (ch == '(') {
                 tokens.add(new Token(TokenType.LPAREN, "(", i));
                 i++;
             } else if (ch == ')') {
                 tokens.add(new Token(TokenType.RPAREN, ")", i));
                 i++;
-            } else { // Unknown character
-                throw new LexicalException("Unexpected character: " + ch + " at position " + i);
+
+            // ----- UNKNOWN CHARACTER -----
+            } else {
+                throw new LexicalException(
+                    "Lexical error: Unexpected character '" + ch + "' at position " + i
+                );
             }
         }
+
         return tokens;
     }
-        
 
-    
-    private void skipWhitespace() { //
+
+    // ----- HELPERS -----
+
+    private void skipWhitespace() {
         while (i < length && Character.isWhitespace(text.charAt(i))) {
             i++;
         }
-    }   
+    }
 
     private boolean isDigit(char ch) {
-        return ch >= '0' && ch <= '9';  
+        return ch >= '0' && ch <= '9';
     }
-    
-    // Read a number (integer or decimal)   
+
+    // Read an integer or decimal number
     private String readNumber() throws LexicalException {
         int start = i;
         boolean hasDot = false;
 
         while (i < length) {
             char ch = text.charAt(i);
+
             if (isDigit(ch)) {
-                i++; // continue reading digits
-            } else if (ch == '.' && !hasDot) {
-                hasDot = true; // allow one dot for decimal point
                 i++;
-            } else {
+            }
+            else if (ch == '.' && !hasDot) {
+                hasDot = true;
+                i++;
+            }
+            else {
                 break;
             }
         }
+
         return text.substring(start, i);
     }
 }
